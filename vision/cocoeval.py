7a5512ad0492ae9742709f6d6e18521aaf0a0c68
Dusan Sulan <46107883+DusanSulan@users.noreply.github.com>
2021-03-05 20:02:17 +0100

    Update cocoeval.py

--- vision/cocoeval.py
+++ vision/cocoeval.py
@@ -425,7 +425,7 @@ class COCOeval:
         Compute and display summary metrics for evaluation results.
         Note this functin can *only* be applied on the default parameter setting
         '''
-        def _summarize( ap=1, iouThr=None, areaRng='all', maxDets=100 ):
+        def _summarize( ap=1, iouThr=None, areaRng='all', maxDets=100, class ):
             p = self.params
             iStr = ' {:<18} {} @[ IoU={:<9} | area={:>6s} | maxDets={:>3d} ] = {:0.3f}'
             titleStr = 'Average Precision' if ap == 1 else 'Average Recall'
@@ -442,14 +442,14 @@ class COCOeval:
                 if iouThr is not None:
                     t = np.where(iouThr == p.iouThrs)[0]
                     s = s[t]
-                s = s[:,:,:,aind,mind]
+                s = s[:,:,class,aind,mind]
             else:
                 # dimension of recall: [TxKxAxM]
                 s = self.eval['recall']
                 if iouThr is not None:
                     t = np.where(iouThr == p.iouThrs)[0]
                     s = s[t]
-                s = s[:,:,aind,mind]
+                s = s[:,class,aind,mind]
             if len(s[s>-1])==0:
                 mean_s = -1
             else:
@@ -460,28 +460,21 @@ class COCOeval:
             avg_ap = 0.0
             num_classes = 10 #np.unique(s[:,:,i,:][2])
             #for i in range(0, num_classes):
-            for i in p.catIds:
-                try:
-                    print('category : {0} : {1}'.format(i,np.mean(s[:,:,i,:])))
-                    avg_ap +=np.mean(s[:,:,i,:])
-            print('(all categories) mAP : {}'.format(avg_ap / num_classes))
+            """for i in p.catIds:
+                print('category : {0} : {1}'.format(i,np.mean(s[:,:,i,:])))
+                avg_ap +=np.mean(s[:,:,i,:])
+            print('(all categories) mAP : {}'.format(avg_ap / num_classes))"""
 
             print(iStr.format(titleStr, typeStr, iouStr, areaRng, maxDets, mean_s))
             return mean_s
         def _summarizeDets():
-            stats = np.zeros((12,))
-            stats[0] = _summarize(1)
-            stats[1] = _summarize(1, iouThr=.5, maxDets=self.params.maxDets[2])
-            stats[2] = _summarize(1, iouThr=.75, maxDets=self.params.maxDets[2])
-            stats[3] = _summarize(1, areaRng='small', maxDets=self.params.maxDets[2])
-            stats[4] = _summarize(1, areaRng='medium', maxDets=self.params.maxDets[2])
-            stats[5] = _summarize(1, areaRng='large', maxDets=self.params.maxDets[2])
-            stats[6] = _summarize(0, maxDets=self.params.maxDets[0])
-            stats[7] = _summarize(0, maxDets=self.params.maxDets[1])
-            stats[8] = _summarize(0, maxDets=self.params.maxDets[2])
-            stats[9] = _summarize(0, areaRng='small', maxDets=self.params.maxDets[2])
-            stats[10] = _summarize(0, areaRng='medium', maxDets=self.params.maxDets[2])
-            stats[11] = _summarize(0, areaRng='large', maxDets=self.params.maxDets[2])
+            stats = np.zeros((4*11,))
+            for i = 1 in range(0,10)
+                stats[0] = _summarize(1, iouThr=.5,  class = i )
+                stats[1] = _summarize(1, iouThr=.5, maxDets=self.params.maxDets[2] , class = i )
+                stats[2] = _summarize(0, iouThr=.5, maxDets=self.params.maxDets[1] , class = i )
+                stats[3] = _summarize(0, iouThr=.5,  maxDets=self.params.maxDets[2] , class = i )
+
             return stats
         def _summarizeKps():
             stats = np.zeros((10,))
