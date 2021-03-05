32dc4a4a7a7c28f6d44212096d5e05e7024f7dd0
Dusan Sulan <46107883+DusanSulan@users.noreply.github.com>
2021-03-05 14:23:39 +0100

    Update cocoeval.py

--- vision/cocoeval.py
+++ vision/cocoeval.py
@@ -4,7 +4,8 @@ import numpy as np
 import datetime
 import time
 from collections import defaultdict
-from . import mask as maskUtils
+    
+from pycocotools import mask as maskUtils
 import copy
 
 class COCOeval:
