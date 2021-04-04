9c9c584ab1a5a8d9abfe910a7bb804a8b671609b
Dusan Sulan <46107883+DusanSulan@users.noreply.github.com>
2021-04-04 16:13:13 +0200

    Update cocoeval.py

--- vision/cocoeval.py
+++ vision/cocoeval.py
@@ -425,7 +425,7 @@ class COCOeval:
         Compute and display summary metrics for evaluation results.
         Note this functin can *only* be applied on the default parameter setting
         '''
-        def _summarize( ap=1, iouThr=None, areaRng='all', maxDets=100, cls=999 ):
+        def _summarize( ap=1, iouThr=None, areaRng='all', maxDets=20, cls=999 ):
             p = self.params
             iStr = ' {:<18} {} @[ IoU={:<9} | area={:>6s} | maxDets={:>3d} ] = {:0.3f}'
             titleStr = 'Average Precision' if ap == 1 else 'Average Recall'
@@ -529,7 +529,7 @@ class Params:
 
         self.iouThrs = np.linspace(.5, 0.95, np.round((0.95 - .5) / .05) + 1, endpoint=True)
         self.recThrs = np.linspace(.0, 1.00, np.round((1.00 - .0) / .01) + 1, endpoint=True)
-        self.maxDets = [1, 10, 100]
+        self.maxDets = [1, 10, 20]
         self.areaRng = [[0 ** 2, 1e5 ** 2], [0 ** 2, 32 ** 2], [32 ** 2, 96 ** 2], [96 ** 2, 1e5 ** 2]]
         self.areaRngLbl = ['all', 'small', 'medium', 'large']
         self.useCats = 1
